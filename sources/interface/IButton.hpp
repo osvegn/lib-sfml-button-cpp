3166e477fe3fcedcaad18291b8c4ac84201f4e0c
Thomas <thoprudhomme@gmail.com>
2022-02-02 15:30:18 +0100

    add IButton file Close #1

---
+++ sources/interface/IButton.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** lib-sfml-button-cpp
+** File description:
+** IButton
+*/
+
+#pragma once
+
+class IButton {
+    public:
+        virtual ~IButton() = default;
+};
