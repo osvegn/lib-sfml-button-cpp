a01721c9a6acd1ef4591f47a63fe54c0ba5f849c
Thomas <thoprudhomme@gmail.com>
2022-02-02 21:39:24 +0100

    NEW : getStat, getText, getShape | Close #9 #8 #7 #5

--- sources/button/Button.cpp
+++ sources/button/Button.cpp
@@ -6,6 +6,18 @@ Button::Button()
     _shape.setOutlineThickness(1);
     _shape.setOutlineColor(sf::Color::Blue);
     _shape.setFillColor(sf::Color(100, 100, 255));
+    _shape.setSize({100, 50});
+    _shape.setPosition({0, 0});
+    _text.setPosition({0, 0});
+    _text.setCharacterSize(16);
+    _text.setFillColor(sf::Color::Black);
+    _text.setString("Button");
+}
+
+Button::Button(std::string fontPath) : Button()
+{
+    _font.loadFromFile(fontPath);
+    _text.setFont(_font);
 }
 
 Button::Button(const Button &b)
@@ -19,6 +31,27 @@ Button &Button::operator=(const Button &b)
     _shape = b._shape;
     _status = b._status;
     return (*this);
+}
+
+void Button::draw(sf::RenderWindow &window) const
+{
+    window.draw(_shape);
+    window.draw(_text);
+}
+
+sf::RectangleShape &Button::getShape(void)
+{
+    return (_shape);
+}
+
+sf::Text &Button::getText(void)
+{
+    return (_text);
+}
+
+Button::stat_e Button::getStat(void) const
+{
+    return (_status);
 }
 
 Button::~Button()
--- sources/button/Button.hpp
+++ sources/button/Button.hpp
@@ -5,17 +5,24 @@
 
 class Button : public IButton {
     public:
-        Button();
-        Button(const Button &);
-        ~Button();
-        Button &operator=(const Button &);
-    protected:
         enum stat_e {
             INACTIVE,
             ACTIVE,
             HOVER,
             CLICK
         };
+        Button();
+        Button(const Button &);
+        Button(std::string fontPath);
+        ~Button();
+        Button &operator=(const Button &);
+        void draw(sf::RenderWindow &window) const;
+        sf::RectangleShape &getShape(void);
+        sf::Text &getText(void);
+        stat_e getStat(void) const;
+    protected:
         stat_e _status;
         sf::RectangleShape _shape;
+        sf::Text _text;
+        sf::Font _font;
 };
